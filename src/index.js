fabf1d8b37f660de1f4debe1fea8c03311221d33
tharveybrown <tharveybrown@gmail.com>
2020-06-02 15:45:28 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks")
+  const submitButton = document.getElementById('submit')
+  
+  submitButton.addEventListener('click', logSubmit)
+  const todosLength = document.querySelectorAll.length
+  
+  function logSubmit(event) {
+    event.preventDefault();
+    const newTask = document.getElementById("new-task-description").value
+    const newItem = document.createElement('li')
+    newItem.innerHTML = newTask 
+    newItem.setAttribute("id", `id_${todosLength}`)
+    const listItem = taskList.appendChild(newItem)
+    
+    const spanDelete = document.createElement("span");
+    spanDelete.setAttribute("id", newItem.id);
+    spanDelete.setAttribute("class", "delete");
+    spanDelete.innerHTML = "&nbsp;&#10007;&nbsp;"
+
+    spanDelete.onclick = deleteItem;
+    listItem.appendChild(spanDelete);
+    clearInput();
+  }
+
+
+  function clearInput() {
+    taskInput.value = ""
+
+  }
 });
+
+
+
+function deleteItem(e) {
+  const id = e.target.id;
+  console.log("delete an item: " + id);
+  document.getElementById(id).remove();
+}         
