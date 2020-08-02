0a3c4a1c705583bfcb77b15dbb0fabc0377dc84d
narduc <radnielpadron@gmail.com>
2020-08-01 18:38:01 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+    creatingTask();
 });
+
+
+const creatingTask = () => {
+    const form = document.getElementById("create-task-form");
+    
+    const contentText = document.getElementById("new-task-description");
+    
+    const todoList = document.getElementById("tasks");
+    form.addEventListener('submit', function(event) {
+          event.preventDefault();
+        let li = document.createElement('li');
+        // let btn = document.createElement('button');
+        // btn.setAttribute('id', 'btn');
+        // btn.innerText = "X";
+        // <button type="button" onclick="alert('Hello world!')">Click Me!</button>
+        // li.setAttribute
+        li.innerText = contentText.value;
+        // li.append(btn);
+        todoList.append(li);
+    })
+
+    // listenToDelete();
+}
+
+
+// const listenToDelete = () => {
+//     const listItems = document.querySelectorAll('ul li i');
+
+//     for ( const li of listItems ){
+//         li.addEventListener('click', function() {
+//             li.parentNode.remove();
+//         })
+//     }
+// }
